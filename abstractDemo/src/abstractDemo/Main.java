c6838663dbdda6e3c1d07df8f685e37f66f4bf81
Omer <Omer@DESKTOP-C6KK9EL>
2022-10-16 14:21:40 +0300

    Final

---
+++ abstractDemo/src/abstractDemo/Main.java
@@ -0,0 +1,18 @@
+package abstractDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager();
+
+		customerManager.databaseManager= new OracleDatabaseManager();
+		customerManager.getCustomer();
+		
+		customerManager.databaseManager= new SqlServerDatabaseManager();
+		customerManager.getCustomer();
+		
+		customerManager.databaseManager= new MySqlDatabasManager();
+		customerManager.getCustomer();
+	}
+
+}
